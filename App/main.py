923a6d7e839a01bf914a395ca160711853f77136
Gughan <gughan_ramakrishnan@hotmail.com>
2022-11-24 21:04:39 +0000

    add files

---
+++ App/main.py
@@ -0,0 +1,99 @@
+import kivy
+
+from kivy.app import App
+from kivy.resources import resource_find
+from kivymd.app import MDApp
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.uix.widget import Widget, WidgetBase
+import time
+from kivy.clock import Clock
+from kivy.animation import Animation
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition, NoTransition, CardTransition
+from kivy.uix.button import Button
+from kivy.graphics import *
+
+Builder.load_file("My.kv")  # Load kivy file into main.py
+
+
+class Background(FloatLayout):
+    pass
+
+
+class InitialScreenDecoration(FloatLayout):
+    pass
+
+
+class InitialScreen(Screen):
+    pass
+
+
+class LoginScreen(Screen):
+    loginBtn = ObjectProperty(None)
+    backBtn = ObjectProperty(None)
+    unInput = ObjectProperty(None)
+    pwInput = ObjectProperty(None)
+
+    def login(self):
+        empty = False
+        for widget_name, widget_object in self.ids.items():
+            if "Input" in widget_name:
+                if len(widget_object.text) == 0:
+                    empty = True
+                    widget_object.helper_text = widget_object.hint_text + " must not be empty"
+        if not empty:
+            print("DO THE LOGIN THINGY")
+
+    def reset_inputs(self):
+        for widget_name, widget_object in self.ids.items():
+            if "Input" in widget_name:
+                widget_object.text = ""
+                widget_object.helper_text = ""
+
+
+class SignUpScreen(Screen):
+    createAccountBtn = ObjectProperty(None)
+    nameInput = ObjectProperty(None)
+    emailInput = ObjectProperty(None)
+    unInput = ObjectProperty(None)
+    pwInput = ObjectProperty(None)
+
+    def create_account(self):
+        empty = False
+        for widget_name, widget_object in self.ids.items():
+            if "Input" in widget_name:
+                if len(widget_object.text) == 0:
+                    empty = True
+                    widget_object.helper_text = widget_object.hint_text+" must not be empty"
+        if not empty:
+            print("DO THE SIGN UP THINGY")
+
+    def reset_inputs(self):
+        for widget_name, widget_object in self.ids.items():
+            if "Input" in widget_name:
+                widget_object.text = ""
+                widget_object.helper_text = ""
+
+
+class FitnessApp(MDApp):
+    x = 700
+    Window.size = (x, x / 9 * 16)
+
+    def on_start(self):
+        pass
+
+    def build(self):
+        self.theme_cls.primary_palette = "Red"
+        self.theme_cls.theme_style = "Light"
+        sm = ScreenManager(transition=SlideTransition())
+        sm.add_widget(InitialScreen(name="initial"))
+        sm.add_widget(SignUpScreen(name="signup"))
+        sm.add_widget(LoginScreen(name="login"))
+        return sm
+
+
+if __name__ == '__main__':
+    FitnessApp().run()
