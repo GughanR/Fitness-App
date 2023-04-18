db9211d814b71cacbe5f97f4dd590a6505aac3a5
Gughan <gughan_ramakrishnan@hotmail.com>
2023-04-18 20:58:58 +0100

    Changed db connection to sqlite

--- Server/database.py
+++ Server/database.py
@@ -3,9 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+mysqldb://root:password@127.0.0.1:3306/test1",
-    echo=True,
-    max_overflow=-1
+engine = create_engine("sqlite:///.fitnessApp.db",
+    echo=True
 )
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
